a958d499ab639d923ad137f8fa075dce12d90ca7
Md Nasim <mohamednasim3108@gmail.com>
2025-09-21 01:43:24 +0530

    Initial Flask app

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, Flask! 🚀 This is running on Render."
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
